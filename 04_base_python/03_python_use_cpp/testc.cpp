ccfc24d5a5517ac0c781538ce6b053d91644138a
houlijia <wohoulijia@163.com>
2025-08-17 16:59:06 +0800

    update c++

--- 04_base_python/03_python_use_cpp/testc.cpp
+++ 04_base_python/03_python_use_cpp/testc.cpp
@@ -1,6 +1,10 @@
 // example.cpp
 extern "C" {
-    int add(int a, int b) {
-        return a + b;
+    int add(int a, int b){
+        return a+b;
     }
 }
+
+
+// g++ -shared -fPIC -o libmath_operations.so math_operations.cpp   --linux
+// g++ -shared -o math_operations.dll math_operations.cpp  --window
