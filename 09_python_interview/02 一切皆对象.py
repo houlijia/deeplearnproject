ea9f85621640b3d8c6cd26c08071b06bf936ab0d
houlijia <wohoulijia@163.com>
2025-08-25 18:45:14 +0800

    update

--- 09_python_interview/02 一切皆对象.py
+++ 09_python_interview/02 一切皆对象.py
@@ -8,3 +8,21 @@ print(l2, type(l2))
 
 d = {"a": "A"}
 print(dict([("a", 1), ("b", 2)]))
+
+
+aaa = [1, 2, 3, 3, 4, 5, 6, 6]
+
+print(aaa)
+a = (1, 2, 3, 1)
+print(a.count(1))
+print(a.index(1))
+
+def foo(x):
+    print(id(x))
+    x.append(5)
+
+x = [1, 2, 3, 4]
+print(id(x))
+foo(x)
+print(x)
+
