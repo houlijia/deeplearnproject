497617e306e8cbf0197a61e25667daf2bf518a6a
houlijia <wohoulijia@163.com>
2025-08-26 21:20:49 +0800

    add interview title

--- 10_interview_record/01_0826_didi_1.py
+++ 10_interview_record/01_0826_didi_1.py
@@ -5,8 +5,11 @@ str = ['flower', 'flow', 'flight']
 def funct(strl):
     res = []
     if len(strl) > 0:
-        for i in zip(*strl):
-            if len(set(i)) == 1:
+        aa = zip(*strl)
+        for i in aa:
+            a011 = set(i)
+            a012 = len(a011)
+            if len(a011) == 1:
                 res.append(i[0])
             else:
                 break
